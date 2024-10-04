80eef83bee92ebc92853ecd0387d8de41cf0ddf4
croraf <vrafaeli@msn.com>
2024-10-04 11:42:02 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Test from '@inxelo/components/lib/AutocompleteLargeDataset2.tsx'
+import Test2 from '@inxelo/components/lib/AutocompleteLargeDataset2'
+
+console.log(Test, Test2);
+
+function App() {
+
+  return (
+    <>
+      
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
